80f7bb692082101539e8a776e375e50057fe2b4b
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-01-15 01:49:42 -0600

    añadir botones y funcinalidades

--- src/intro.py
+++ src/intro.py
@@ -1,1 +1,40 @@
-from PyQt5 import *
+from PyQt5 import uic
+from PyQt5.QtWidgets import * 
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtGui import * 
+from PyQt5.QtCore import *
+import sys
+import pandas as pd
+
+class IntroScreen(QWidget):
+    def __init__(self):
+        super(IntroScreen, self).__init__()
+        uic.loadUi('intro.ui', self)
+
+        ## Buttons
+        self.mecatronica_button = self.findChild(QPushButton, 'mecatronica_button')
+        self.biomedica_button = self.findChild(QPushButton, 'biomedica_button')
+        self.open_file_button = self.findChild(QPushButton, 'open_file_button_button')
+
+        ## Button actions
+        self.mecatronica_button.clicked.connect(self.mecatronica_button_click)
+        self.biomedica_button.clicked.connect(self.biomedica_button_click)
+        self.open_file_button.clicked.connect(self.open_file)
+        
+        self.show()
+
+    def mecatronica_button_click(self):
+        print('mecatronica')
+
+    def biomedica_button_click(self):
+        print('biomedica')
+
+    def open_file(self):
+        file, _ = QFileDialog.getOpenFileName(self, 'Open File', 'c:\\', 'Excel Files (*.xlsx)')
+
+        df = pd.DataFrame(pd.read_excel(file))
+        print(df)
+
+app = QApplication(sys.argv) 
+window = IntroScreen() 
+app.exec_() 
