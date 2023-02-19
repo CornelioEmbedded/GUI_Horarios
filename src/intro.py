2bcb64edfb0632d6bbb5733ab78273c007043eef
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-02-19 14:50:01 -0600

    make order class method to order list_dict

--- src/intro.py
+++ src/intro.py
@@ -108,6 +108,7 @@ class IntroScreen(QWidget):
             list_of_classes, current_text = convertion.find_class(self.subject_menu.currentText(), self.string_classes)
             self.cleaned_list_of_classes = convertion.clean_list_of_classes(list_of_classes)
             self.list_dict = convertion.get_classes_data(self.cleaned_list_of_classes)
+            self.order_classes(self.list_dict)
             self.display_classes(self.list_dict)
         except IndexError:
             pass
@@ -160,6 +161,32 @@ class IntroScreen(QWidget):
                 if row != 0 and column != 0:
                     widget.setStyleSheet("")
                     widget.setText("")
+
+    def order_classes(self, list_dict):
+        expected_ordered = list(self.rows.keys())
+        list_135 = [] #135 means Monday=1, Wednesday=3, Friday=5
+        list_24 = [] #24 means Tuesday=2, Thursday=4
+        new_order_135 = []
+        new_order_24 = []
+        for dict in list_dict:
+            if '135' in dict.values():
+                new_order_135.append(expected_ordered.index(dict['Hour']))
+                list_135.append(dict)
+            elif '2' in dict.values() or '4' in dict.values():
+                new_order_24.append(expected_ordered.index(dict['Hour']))
+                list_24.append(dict)
+        print(f'List 135: {sorted(new_order_135)}')
+        print(f'List 24: {sorted(new_order_24)}')
+        order_by_day = list_135 + list_24
+        for item in order_by_day:
+            print(item)
+        #     hour = dict['Hour']
+        #     day = dict['Day']
+        #     new_order.append(expected_ordered.index(hour))
+        #     print(f'hora: {hour}, dia: {day}, index: {expected_ordered.index(hour)}')
+        # sorted_order = sorted(new_order)
+        # print(sorted_order)
+
 
 
 app = QApplication(sys.argv) 
