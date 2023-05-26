58259c123071577b1f611dd1fc06f11d7680256a
Cornelio Posadas P <posadascornelio38@gmail.com>
2023-05-25 18:19:31 -0600

    trying to connect intro with main screens

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import sys
 import pandas as pd
+from intro import IntroScreen
 
 
 class MainScreen(QMainWindow):
@@ -11,10 +12,41 @@ class MainScreen(QMainWindow):
         super(MainScreen, self).__init__()
         uic.loadUi(r'ui_files\main.ui', self)
 
+        ## Buttons
+        self.subjects_button = self.findChild(QPushButton, 'subjects')
+        self.professor_button = self.findChild(QPushButton, 'professors')
+        self.modifying_button = self.findChild(QPushButton, 'modifying')
+        self.information_button = self.findChild(QPushButton, 'information')
+        self.configuration_button = self.findChild(QPushButton, 'configuration')
+
+        ##Buttons actions
+        self.subjects_button.clicked.connect(self.subjects_button_click)
+        self.professor_button.clicked.connect(self.professors_button_click)
+        self.modifying_button.clicked.connect(self.modifying_button_click)
+        self.information_button.clicked.connect(self.information_button_click)
+        self.configuration_button.clicked.connect(self.configuration_button_click)
+        
         ## Initialize functions
         self.show()
+    
+    def subjects_button_click(self):
+        spot = self.findChild(QFrame, 'frame_4')
+        new_spot = QVBoxLayout(spot)
+        new_spot.addWidget(IntroScreen())
+    
+    def professors_button_click(self):
+        print('professors')
+    
+    def modifying_button_click(self):
+        print('modifying')
+    
+    def information_button_click(self):
+        print('information')
+    
+    def configuration_button_click(self):
+        print('configuration')
 
 
-app = QApplication(sys.argv) 
+app = QApplication(sys.argv)
 window = MainScreen() 
 app.exec_() 
